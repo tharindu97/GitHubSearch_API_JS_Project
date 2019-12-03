7a023da2e028e913579a6e2ca3b9ab39fc04d4b8
tharindu97 <tharindukavishna@gmail.com>
2019-12-03 18:15:26 +0530

    completed_have_one_error

--- github.js
+++ github.js
@@ -2,14 +2,19 @@ class Github {
     constructor(){
         this.client_id = 'dc9af257b9663fd0ac32';
         this.client_secret = '5ac89454e3d00a97d9336fe5f6ed18212eac09de';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user){
         const profileResponse = await fetch (`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const repoResponse = await fetch (`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`);
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return{
-            profile
+            profile,
+            repos
         }
 
     }
--- ui.js
+++ ui.js
@@ -14,8 +14,8 @@ class UI{
             <div class="col-md-9">
                 <span class="badge badge-primary">Public Repos:${user.public_repos}</span>
                 <span class="badge badge-secondary">Public Gists:${user.public_gists}</span>
-                <span class="badge badge-success">Followers:${user.public_followers}</span>
-                <span class="badge badge-info">Following:${user.public_following}</span>
+                <span class="badge badge-success">Followers:${user.followers}</span>
+                <span class="badge badge-info">Following:${user.following}</span>
                 <br><br>
                 <ul class="list-group">
                     <li class="list-group-item">Company:${user.company}</li>
@@ -31,9 +31,35 @@ class UI{
         `;
     }
 
+    // Show user repos
+    showRepos(repos) {
+        let output = '';
+        repos.forEach(function(repo){
+            output += `
+                <div class="card card-body mb-2">
+                    <div class="row">
+                        <div class="col-mb-6">
+                            <a href="${repo.html_url}" target="_blank" >${repo.name}</a>
+                        </div>
+                        <div class="col-mb-6">
+                            <span class="badge badge-primary">Stars:${repo.stargazers_count}</span>
+                            <span class="badge badge-secondary">Watchers:${repo.watchers_count}</span>
+                            <span class="badge badge-success">Forks:${repo.forms_count}</span>
+                        
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+        // Output repos
+        document.getElementById('repos').innerHTML = output;
+        
+    }
+
     //show alert message
     showAlert(message, className){
-
+        //Create any remainig alerts
+        this.clearAlert();
         //Create div
         const div = document.createElement('div');
         //Add class
@@ -47,7 +73,21 @@ class UI{
         //insert alert
         container.insertBefore(div, search);
 
+        // Timeout after 3 sec
+        setTimeout(()=>{
+            this.clearAlert();
+        },3000);
+
          
+    }
+
+    // Clear alert message
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+
+        if(currentAlert){
+            currentAlert.remove();
+        }
     }
     //Clear profile
     clearProfile(){
